6e86208e801209ec1c4eec2df633a2c6a86bec4a
aisehiwebwala <116726737+aisehiwebwala@users.noreply.github.com>
2024-07-19 00:32:56 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const upload = multer();
+const app = express();
+const port = 3000;
+
+app.use("/file", express.static(__dirname + "/file"));
+
+app.get("/upload-file", (req, res) => {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
+app.post("/upload-file", upload.single("phile"), async (req, res) => {
+  try {
+    const file = req.file;
+    fs.writeFileSync(__dirname + "/file/" + file.originalname, file.buffer);
+    res.redirect("/upload-file");
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
